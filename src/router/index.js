78f881f4ef7c1a4b22e4bcd62dad8f35203c4d11
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-03 21:09:33 +0300

    *Refactored routes

--- src/router/index.js
+++ src/router/index.js
@@ -28,15 +28,15 @@ const routes = [
     name: "singleblog",
     component: Singleblog,
   },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../Pages/AboutView.vue"),
-  },
+  // {
+  //   path: "/about",
+  //   name: "about",
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ "../Pages/AboutView.vue"),
+  // },
 ];
 
 const router = new VueRouter({
