6e449bf9a7b4ce1c4a4a8fdc761a2aaf642a5b2c
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-03 19:52:29 +0300

    *Added header and blogcard componet to  homepage

---
+++ src/Pages/Homepage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Header />
+    <div class="flex flex-wrap p-5">
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+    </div>
+  </div>
+</template>
+<script>
+import Header from "@/components/Header.vue";
+import BlogCard from "@/components/BlogCard.vue";
+export default {
+  name: "HomePage",
+  components: { Header, BlogCard },
+};
+</script>
