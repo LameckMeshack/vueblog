af36a90dce9687198046e22da99a42b1ab2c3b5f
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-04 01:31:11 +0300

    *Added a loader in the blog container

--- src/components/BlogsContainer.vue
+++ src/components/BlogsContainer.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="flex flex-wrap p-5">
+    <p v-if="loading"><Loading /></p>
     <BlogCard
       v-for="blog in blogs"
       :key="blog.id"
@@ -7,11 +8,13 @@
       :content="blog.body"
       :id="blog.id"
     />
+    <p v-if="error">{{ error }}</p>
   </div>
 </template>
 <script>
 import BlogCard from "./BlogCard.vue";
 import axios from "axios";
+import Loading from "./Loading.vue";
 export default {
   name: "BlogsContainer",
   data() {
@@ -23,15 +26,15 @@ export default {
   //   required: true,
   // },
   //   },
-  components: { BlogCard },
+  components: { BlogCard, Loading },
   mounted: function () {
     this.loading = true;
     axios
-      .get("https://jsonplaceholder.typicode.com/posts")
+      .get("http://localhost:3000/blogs")
       .then((res) => {
         this.loading = false;
         this.blogs = res.data;
-        // console.log(this.blogs);
+        console.log(this.blogs);
       })
       .catch((err) => {
         this.loading = false;
