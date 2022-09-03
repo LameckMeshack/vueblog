6985b1b4ff00ec7f4382fd44512e629bf2ca83a3
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-03 19:57:14 +0300

    *Rmoved default app css

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,19 @@
 <template>
   <div id="app">
-    <nav>
+    <!-- <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </nav>
+    </nav> -->
+    <Navbar />
     <router-view />
     <Footer />
   </div>
 </template>
 <script>
 import Footer from "./components/Footer.vue";
+import Navbar from "./components/Navbar.vue";
 export default {
   name: "App",
-  components: { Footer },
+  components: { Footer, Navbar },
 };
 </script>
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
